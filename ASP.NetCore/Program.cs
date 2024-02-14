64d10e6e2b3c8019e9309e1abf2a3ba7357fa070
Brajesh <brajesh@datagainservices.com>
2024-02-14 08:34:15 +0530

    git Ignore

---
+++ ASP.NetCore/Program.cs
@@ -0,0 +1,15 @@
+using Asp.NetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateWebHostBuilder(args).Build().Run();
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+}
