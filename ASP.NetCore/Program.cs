296687c09249401f9565c1b30a73301484affca3
Brajesh Mourya <104342173+briz1007@users.noreply.github.com>
2024-02-15 22:59:41 +0530

    Update Program.cs

--- ASP.NetCore/Program.cs
+++ ASP.NetCore/Program.cs
@@ -5,9 +5,13 @@ using Microsoft.AspNetCore.Hosting;
 public class Program
 {
     public static void Main(string[] args)
-    {        
+    {
+        //changes by bipin
         CreateWebHostBuilder(args).Build().Run();
+        //changes by b2
         //Changes by Harshdeep
+        //changes by ANuj
+        //changes by b1
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
