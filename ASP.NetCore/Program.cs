adc5d0a3410f185844248f4e7ccbcfa4ea510886
Brajesh <brajesh@datagainservices.com>
2024-02-15 22:27:33 +0530

    Changes 2

--- ASP.NetCore/Program.cs
+++ ASP.NetCore/Program.cs
@@ -7,7 +7,7 @@ public class Program
     public static void Main(string[] args)
     {        
         CreateWebHostBuilder(args).Build().Run();
-        //git changes
+        //git changes by brajesh
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
